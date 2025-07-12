bda2af79b0ffcdd3e15db599540a5c08ddd8ea93
Vaguiner Gonzalez <cardealrusso@vaguiner.eu.org>
2025-07-12 19:29:51 +0000

    Init

---
+++ README.md
@@ -0,0 +1,33 @@
+## Maximum Performance TinyCore Packages
+
+### Flags
+
+- **`-Ofast`**  
+  Enables all `-O3` optimizations plus unsafe math and standard-breaking transforms (e.g., assumes no NaNs or signed overflow).
+
+- **`-march=alderlake -mtune=alderlake`**  
+  Uses all CPU features from Alder Lake.
+
+- **`-fmerge-all-constants`**  
+  Deduplicates identical constants (arrays, strings, etc.) across translation units to reduce size and improve cache efficiency.
+
+- **`-fno-semantic-interposition`**  
+  Assumes symbols are not replaced at runtime, allowing cross-module inlining and better optimization of function calls and globals.
+
+- **`-ftree-vectorize`**  
+  Enables vectorization at the GIMPLE tree level for both loops and straight-line code, using SIMD instructions where possible.
+
+- **`-fipa-pta`**  
+  Performs whole-program pointer analysis across functions to improve alias detection and unlock more aggressive optimization.
+
+- **`-funroll-loops`**  
+  Fully unrolls loops with constant bounds to remove loop control overhead and increase ILP (instruction-level parallelism).
+
+- **`-floop-nest-optimize`**  
+  Enables Pluto-based loop restructuring using ISL to improve cache locality and expose parallelism. Experimental.
+
+---
+
+### Warning
+
+These flags prioritize **performance** over **portability** and **standards compliance**.
---
+++ scripts/micropython/build.sh
@@ -0,0 +1,39 @@
+sudo rm -f /bin/uname
+echo '#!/bin/busybox ash
+case "$1" in
+  -r)
+    echo "6.12.11-tinycore64"
+    ;;
+  -m)
+    echo "x86_64"
+    ;;
+  -s)
+  echo "Linux"
+  ;;
+  -v)
+  echo "#1 SMP Sun Jan 26 16:50:13 UTC 2025"
+  ;;
+  *)
+    echo "6.12.11-tinycore64"
+    ;;
+esac' | sudo tee /bin/uname > /dev/null
+sudo chmod +x /bin/uname
+tce-load -lwi compiletc libffi-dev python3.9 squashfs-tools jq upx submitqc curl sstrip
+
+workdir=$(mktemp -d)
+cd $workdir
+version=1.25.0
+
+wget --no-check-certificate -O- https://github.com/micropython/micropython/releases/download/v$version/micropython-$version.tar.xz | tar -xJ
+cd micropython-$version/ports/unix/
+#for some reason, the first make with cflags causes error. you need to compile with just "make" then pass the flags
+make
+make clean
+CFLAGS="-fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -Ofast -march=alderlake -mtune=alderlake -flto" CXXFLAGS="-fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -Ofast -march=alderlake -mtune=alderlake -flto" make -j4
+bindir=$(mktemp -d)
+mkdir -p $bindir/usr/local/bin/
+mv build-standard/micropython $bindir/usr/local/bin/
+sstrip -z $bindir/usr/local/bin/micropython
+mksquashfs $bindir micropython.tcz
+sudo submitqc --nonet --blocksize=65536 micropython.tcz
+mv -f micropython.tcz /output
