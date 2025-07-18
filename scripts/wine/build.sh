7d98c279b76425721945a6aac5330d34f1e8ddf3
Vaguiner Gonzalez <cardealrusso@vaguiner.eu.org>
2025-07-18 21:17:39 +0000

    Update build.sh

--- scripts/wine/build.sh
+++ scripts/wine/build.sh
@@ -55,8 +55,8 @@ export PATH=$PATH:./llvm-mingw-20250709-ucrt-ubuntu-22.04-x86_64/bin/
 sudo ln -s /lib /lib64
 
 #compile
-export CFLAGS="-fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -O3 -march=alderlake -mtune=alderlake"
-export CXXFLAGS="-fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -O3 -march=alderlake -floop-nest-optimize -mtune=alderlake"
+export CFLAGS="-fopt-info-vec-optimized -fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -O3 -march=alderlake -mtune=alderlake"
+export CXXFLAGS="-fopt-info-vec-optimized -fmerge-all-constants -fno-semantic-interposition -ftree-vectorize -fipa-pta -funroll-loops -floop-nest-optimize -O3 -march=alderlake -floop-nest-optimize -mtune=alderlake"
 ./configure --libdir=/usr/local/lib --prefix=/usr/local --localstatedir=/var --without-dbus --enable-archs=i386,x86_64
 find . -name Makefile -type f -exec sed -i 's/-g -O2/-O3 -march=alderlake -mtune=alderlake/g' {} \;
 
